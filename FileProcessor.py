96fe4741050785a4ceeceecd2c21184d427ed83e
Mykyta Kamak <kamaknikita@gmail.com>
2020-05-24 11:59:03 +0300

    Edit xml load to file in pretty format

--- FileProcessor.py
+++ FileProcessor.py
@@ -1,4 +1,5 @@
 import xml.etree.ElementTree as ET
+from xml.dom import minidom
 
 
 class XmlExporter:
@@ -7,16 +8,20 @@ class XmlExporter:
     def __init__(self, commands_roadmap):
         self.commands_roadmap = commands_roadmap
 
+    def __prettify(self, elem):
+        """Return a pretty-printed XML string for the Element.
+        """
+        rough_string = ET.tostring(elem)
+        doc = minidom.parseString(rough_string)
+        dt = minidom.getDOMImplementation('').createDocumentType('scenario', '', 'sipp.dtd')
+        doc.insertBefore(dt, doc.documentElement)
+        return doc.toprettyxml(indent = "  ", encoding = 'ISO-8859-1').decode('utf-8')
+
     def loadToFile(self, path):
         root = ET.Element('scenario')
         root.set('name', 'test1')
-
         for cmd in self.commands_roadmap:
             root.append(cmd.convertToXmlView())
-
-        with open(path, 'wb') as f:
-            f.write('<?xml version="1.0" encoding="UTF-8" ?>\n<!DOCTYPE tmx SYSTEM "tmx14a.dtd">'.encode('utf8'))
-            # tree = ET.ElementTree(root)
-            # tree.write(f, 'utf-8')
-            f.write(ET.tostring(root))
+        with open(path, 'w') as f:
+            f.write(self.__prettify(root))
 
